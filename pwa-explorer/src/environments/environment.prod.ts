ba40a527dff573063e39d8a870dfadbb123bba8e
Erik Norfelt <erik.norfelt@monitor.se>
2026-03-10 11:00:03 +0100

    updated vapid public key in prod

--- pwa-explorer/src/environments/environment.prod.ts
+++ pwa-explorer/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  vapidPublicKey: '',
+  vapidPublicKey: 'BFzLdWxhZR_YGb1306DYikZ8MNFDcn7Ze90xxdKj4wQ-nlTPf21zovE861OTfFw09f_Zm72m22l1mBTeBM2ucaA',
 };
